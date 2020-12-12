462734422d69ae02bef0697cf7e55a7b519c3d2b
anongtf <anongtf26@gmail.com>
2020-12-13 01:40:08 +0700

    add pwa

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 module.exports = {
-  "transpileDependencies": [
-    "vuetify"
-  ]
+    "transpileDependencies": [
+        "vuetify"
+    ],
+    pwa: {
+        themeColor: '#a5a5a5',
+        msTileColor: '#2b5797',
+        iconPaths: {
+            msTileImage: 'img/icons/mstile-150x150.png'
+        }
+    }
 }
